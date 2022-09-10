c0f24b5f0844a7bd648c74c5912812eeadb31c1c
”Dmitri <dmitri.zd1958@gmail.com>
2022-09-10 21:46:19 +0200

    Add sudoku interfaces

---
+++ src/common/interfaces.ts
@@ -0,0 +1,3 @@
+export type Maybe<T> = T | null
+
+export type ListOfNine<T> = [T, T, T, T, T, T, T, T, T]
---
+++ src/common/sudoku.ts
@@ -0,0 +1,14 @@
+import { ListOfNine, Maybe } from "./interfaces";
+
+export type Board = ListOfNine<ListOfNine<Cell>>
+
+export type Cell = Maybe<CellWithValue | PossibleCellValue>
+
+export type CellValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+export type PossibleCellValue = ListOfNine<Maybe<CellValue>>
+
+export interface CellWithValue {
+  value: CellValue
+  isGenerated?: boolean
+}
